2b2a709fc2b1738eb25a0d7fb94363ff2d9ae148
Sergiu Stancioiu <s.sergiu96@gmail.com>
2026-03-03 21:01:17 +0200

    Dynamically added news and articles

--- README.md
+++ README.md
@@ -6,10 +6,14 @@
 
 Thanks for checking out this front-end coding challenge.
 
+## Usefull links
+
+<https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch>
+
 ## Day 1
 
 Created a new repository for this project and pushed the code. Completed the mobile design. (2.5 hours)
 
 ## Day 2
 
-Worked on mobile menu design and functionality, added hover state for links and buttons. (1.2 hours)
+Worked on the mobile menu design and functionality, added hover states for links and buttons, and dynamically added news and articles. (2 hours)
---
+++ assets/js/articles.js
@@ -0,0 +1,55 @@
+async function getData() {
+  const url = '../../public/data.json';
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    let newsData = result.news;
+    let articlesData = result.articles;
+
+    console.log(result);
+    console.log('articlesData', articlesData);
+
+    for (const news of newsData) {
+      newsCard(news);
+    }
+
+    for (const article of articlesData) {
+      articleCard(article);
+      console.log('article', article);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getData();
+
+function newsCard(news) {
+  const sidebarContainer = document.querySelector('.sidebar-container');
+  const sidebarTemplate = document.querySelector('.sidebar-template');
+
+  const clone = sidebarTemplate.content.cloneNode(true);
+
+  clone.querySelector('.news-title').innerText = news.title;
+  clone.querySelector('.news-text').innerText = news.text;
+
+  sidebarContainer.appendChild(clone);
+}
+
+function articleCard(article) {
+  const articlesListContainer = document.querySelector(
+    '.articles-list-container',
+  );
+  const articleTemplate = document.querySelector('.article-template');
+
+  const clone = articleTemplate.content.cloneNode(true);
+
+  clone.querySelector('.article-title').innerText = article.title;
+  clone.querySelector('.article-text').innerText = article.text;
+
+  articlesListContainer.appendChild(clone);
+}
